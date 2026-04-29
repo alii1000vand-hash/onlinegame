a18be6078808f03b113778bf677b5da03b736092
alii1000vand-hash <alii1000vand@gmail.com>
2026-04-29 12:34:19 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 export const config = { runtime: "edge" };
 
-const TARGET_BASE = (process.env.TARGET_DOMAIN || "").replace(/\/$/, "");
+const TARGET_BASE = (process.env.SALAM || "").replace(/\/$/, "");
 
 const STRIP_HEADERS = new Set([
   "host",
@@ -20,7 +20,7 @@ const STRIP_HEADERS = new Set([
 
 export default async function handler(req) {
   if (!TARGET_BASE) {
-    return new Response("Misconfigured: TARGET_DOMAIN is not set", { status: 500 });
+    return new Response("Misconfigured: SALAM is not set", { status: 500 });
   }
 
   try {
